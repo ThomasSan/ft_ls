f18048be5feea6847cac2ba95ca12893f0774299
Thomas SANZEY <tsanzey@e2r6p11.42.fr>
2015-12-28 15:12:50 +0100

    push du .h et des infos fichiers

--- ls/main.c
+++ ls/main.c
@@ -6,12 +6,20 @@
 /*   By: tsanzey <tsanzey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/27 10:58:57 by tsanzey           #+#    #+#             */
-/*   Updated: 2015/12/27 11:25:44 by tsanzey          ###   ########.fr       */
+/*   Updated: 2015/12/28 14:09:53 by tsanzey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <dirent.h>
+#include <sys/stat.h>
 #include "libft/libft.h"
+#include "ft_ls.h"
+
+int		ft_parse_option()
+{
+	
+	return (0);
+}
 
 int		main(int ac, char **av)
 {
@@ -19,12 +27,16 @@ int		main(int ac, char **av)
 	DIR *dirp;
 	struct dirent *dp;
 
-	dirp = opendir(av[1]);
+	if (ac == 1)
+		dirp = opendir(".");
+	else
+		dirp = opendir(av[1]);
 	if (dirp == NULL)
 		return (0);
 	while ((dp = readdir(dirp)) != NULL)
 	{
-		ft_putendl_fd(dp->d_name, 1);
+		ft_inspect_file(dp->d_name);
+		ft_putendl(dp->d_name);
 	}
 	closedir(dirp);
 	return (0);
